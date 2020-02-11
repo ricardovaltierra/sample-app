bda106454775eb948885920fadfe7fd2563d7539
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-02-11 10:52:15 -0600

    added gravatar_helper / users resource in routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+  
+  # create  C
   def new
+    debugger
   end
+
+  # read    R
+  def show
+    @user = User.find(params[:id])        
+  end
+  
+  # update  U
+  # delete  D
+
 end
