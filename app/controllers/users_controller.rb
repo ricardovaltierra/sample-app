2a6f7dff44d985bdce70f8e41635c9b834796e36
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-02-17 19:17:44 -0600

    added user_path to routs file (edit) / added update action to controller / updated edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -25,9 +25,17 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
   
   # delete  D
-
 
   # private methods
   private
