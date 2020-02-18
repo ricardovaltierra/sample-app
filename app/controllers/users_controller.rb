48d0155d7fa1de8a34d2fbe4732d9e54ed4fc8b2
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-02-18 14:12:43 -0600

    added firendly forwarding (redirect users logged - not logged / integration test methods added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
-class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+class UsersController < ApplicationController  
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   # create  C
   def new
@@ -18,17 +19,20 @@ class UsersController < ApplicationController
   end
 
   # read    R
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])        
   end
   
   # update  U
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -52,8 +56,15 @@ class UsersController < ApplicationController
   # Confirms a logged-in user.
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])    
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
