dd58a2d55afbd67d7b7a5f109f16311593f28626
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-02-19 14:21:43 -0600

    added filters to index and destroy / tdd index and destroy / seed created with user layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController  
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   # create  C
   def new
@@ -42,13 +43,18 @@ class UsersController < ApplicationController
   end
   
   # delete  D
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
   # private methods
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :admin)
   end
 
   # Before filters
@@ -66,5 +72,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])    
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
