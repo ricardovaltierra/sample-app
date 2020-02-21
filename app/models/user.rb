1694fbf731785a73257ef0d3c9b01ed920cf17f8
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-02-20 18:44:57 -0600

    added routes foor mail server / account activation html&text refactored / enviroment setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord  
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
-  before_create :create_activation_digest
-  
-  before_save { email.downcase! }
+  before_create :create_activation_digest  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
