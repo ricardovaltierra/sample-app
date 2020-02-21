d132153e7b40998a025417dff071ba7d6338b05f
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-02-21 16:11:08 -0600

    refactored of 'authenticated' method / tdd for reactoring / edit action implememnted for mail auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord  
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
-  before_create :create_activation_digest  
+  before_create :create_activation_digest
+
+  before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
@@ -31,9 +34,10 @@ class User < ApplicationRecord
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Forgets a user.
