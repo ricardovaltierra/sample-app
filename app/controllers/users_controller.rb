d3d11c4bf8f50927fc7bcfea07441b04d5322599
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-03-02 21:56:05 -0600

    micropost feed and posting added for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController  
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]  
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -26,8 +26,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated
-  end
+    @microposts = @user.microposts.paginate(page: params[:page])
+    #redirect_to root_url and return unless @user.activated
+  end  
   
   # update  U
   def edit
@@ -51,16 +52,15 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  # Before filters
+  # private methods
+  private
 
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
+
+  # Before filters
 
   # Confirms the correct user.
   def correct_user
@@ -71,13 +71,5 @@ class UsersController < ApplicationController
   # Confirms an admin user.
   def admin_user
     redirect_to(root_url) unless current_user.admin?
-  end
-
-  # private methods
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
   end
 end
