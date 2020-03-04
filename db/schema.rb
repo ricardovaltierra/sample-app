91afa9f6c496c163bf2a36db6d3bc4d299255045
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-03-04 05:46:14 -0600

    Add user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200302191550) do
+ActiveRecord::Schema.define(version: 20200303192434) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
